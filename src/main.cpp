6de86c561454370433c7746a9462909638962ece
Shestakov Artem <art.shestakov@icloud.com>
2023-10-05 09:22:44 +0300

    Добавлен скелет проекта

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <boost/program_options/options_description.hpp>
+//-----------------------------------------------------------------------------
+int main()
+{
+    return EXIT_SUCCESS;
+}
+//-----------------------------------------------------------------------------
